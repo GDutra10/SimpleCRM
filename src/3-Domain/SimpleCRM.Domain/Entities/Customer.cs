0fe94b879c34043b36baa60a5ddae4ca345f11e6
Guilherme Dutra Alves <gui_dutralves@hotmail.com>
2023-05-26 23:35:37 -0300

    fix warning

--- src/3-Domain/SimpleCRM.Domain/Entities/Customer.cs
+++ src/3-Domain/SimpleCRM.Domain/Entities/Customer.cs
@@ -9,7 +9,7 @@ public class Customer : Record, IDbRecord
     public string Email { get; internal set; } = default!;
     public string Telephone { get; internal set; } = default!;
     public Guid UserId { get; internal set; }
-    public User User { get; internal set; }
+    public User User { get; internal set; } = default!;
     public InteractionState State { get; internal set; }
     
     private Customer(){ }
